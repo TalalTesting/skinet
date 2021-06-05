1837fc92a82b30bf3087a7f919b96e158d99d7cb
TalalTesting <talal.testing@gmail.com>
2021-06-05 23:01:07 +0300

    InitialCommit

---
+++ Skinet.Api/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Skinet.Infrastructure.Data;
+using Skinet.Core.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Skinet.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("GetProducts")]
+        public async Task<ActionResult<List<Product>>> GetProductsAsync()
+        {
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet]
+        [Route("GetProduct{id}")]
+        public async Task<ActionResult<Product>> GetProductAsync(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            return Ok(product);
+        }
+    }
+}
