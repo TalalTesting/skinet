28e0d083f1223689b95929cbf10050ed6a58d39c
TalalTesting <talal.testing@gmail.com>
2021-08-31 23:06:29 +0300

    End of section 19

--- client/src/app/checkout/checkout.service.ts
+++ client/src/app/checkout/checkout.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { CoreEnvironment } from '@angular/compiler/src/compiler_facade_interface';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { IDeliveryMethod } from '../shared/models/deliveryMethod';
+import { map } from 'rxjs/operators';
+import { IOrderToCreate } from '../shared/models/order';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CheckoutService {
+  baseUrl = environment.apiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  createOrder(order: IOrderToCreate) {
+    return this.http.post<IOrderToCreate>(this.baseUrl + 'orders', order);
+  }
+
+  getDeliveryMethods() {
+    return this.http.get<IDeliveryMethod[]>(this.baseUrl + 'orders/deliveryMethods').pipe(
+      map((dm: IDeliveryMethod[]) => {
+        return dm.sort((a, b) => b.price - a.price);
+      })
+    );
+  }
 }
